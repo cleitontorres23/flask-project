e57a9f745b3be46c3cc28c49e5b5f58ac964c79b
cleitontorres23 <cleiton.torres23@gmail.com>
2023-04-24 10:09:14 -0300

    feat: create gitignore

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,4 +1,15 @@
 /* Global document */
 
-var myheading = document.querySelector('h1')
-myheading.textContent = 'Be kind'   
+//var myheading = document.querySelector('h1')
+//myheading.textContent = 'Be kind'   
+
+var dogImage = document.querySelector('img');
+
+dogImage.onclick = function () {
+    var myImages = dogImage.getAttribute('src');
+    if (myImages ==='images/funny-puppet-dog.jpg') {
+        dogImage.setAttribute ('src', 'images/puppet-dog-brown.jpg');
+}   else {
+        dogImage.setAttribute ('src', 'images/puppet-dog.jpg');
+    }
+}
